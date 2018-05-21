5f5ce36f16433b69da88733d314c1a0fcc8ca989
luutry <luutry@163.com>
2018-05-21 13:51:58 +0800

    branch

---
+++ src/main/java/com/example/sell/repository/ProductInfoRepository.java
@@ -0,0 +1,9 @@
+package com.example.sell.repository;
+
+import com.example.sell.ProductInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductInfoRepository extends JpaRepository<ProductInfo, String>{
+
+
+}
